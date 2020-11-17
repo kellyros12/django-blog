b72f35b00eca570b094558dbb434cc5ab8f41681
Kelly Ros <kellyros91@gmail.com>
2020-11-16 20:59:06 -0800

    Completed assignment 8(?)

--- blogging/views.py
+++ blogging/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def detail_view(request, post_id):
+class BlogListView(ListView):
     published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+    queryset = published.order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "blogging/detail.html"
+
+    def post(self, request, *args, **kwargs):
+        published = Post.objects.exclude(published_date__exact=None)
+
+        post =  published.get(pk=post_id)
+
+        context = {"object": post}
+        return render(request, "blogging/detail.html", context)
+
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
+#
+#
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
